e39706dd878be156e503dc1788b1a460f76cf867
dusixian <1912647548@qq.com>
2024-11-12 00:13:07 -0800

    fix dataloader to one_hot

--- dataloader.py
+++ dataloader.py
@@ -8,9 +8,11 @@ from torch.utils.data import DataLoader
 # constants
 END_TOKEN = "E"
 PAD_TOKEN = "P"
-MAX_SEQ_LENGTH = 100 # TODO: @haoyu, check whether this is a good value
+MAX_SEQ_LENGTH = 1000 # TODO: @haoyu, check whether this is a good value
 file_names = ["ADAR1_seq.txt", "ADAR2_seq.txt", "ADAR3_seq.txt", "Endogenous_ADAR1_seq.txt"]
 ADAR_types = ["ADAR1", "ADAR2", "ADAR3", "Endogenous_ADAR1"]
+vocabulary = {'A': 0, 'T': 1, 'C': 2, 'G': 3, 'E': 4, 'P': 5}
+vocab_size = len(vocabulary)
 
 def load_rna_pairs(file_path, ADAR_type):
     data = pd.read_csv(file_path, sep='\t', header=None, 
@@ -38,6 +40,18 @@ def load_rna_pairs(file_path, ADAR_type):
     
     return pairs
 
+def one_hot_encode(sequence, max_seq_length):
+    indices = [vocabulary[char] for char in sequence]
+    # Padding if necessary
+    
+    if len(indices) < max_seq_length:
+        indices += [vocabulary['P']] * (max_seq_length - len(indices))
+    else:
+        indices = indices[:max_seq_length]
+
+    one_hot_seq = torch.nn.functional.one_hot(torch.tensor(indices), num_classes=vocab_size)
+    return one_hot_seq.float() 
+
 class RnaPairDataset(Dataset):
     def __init__(self, file_names, ADAR_types, max_seq_length=MAX_SEQ_LENGTH, pad_token=PAD_TOKEN):
         rnn_pairs = []
@@ -54,8 +68,8 @@ class RnaPairDataset(Dataset):
 
     def __getitem__(self, idx):
         pair = self.rnn_pairs[idx]
-        seq1 = self.pad_sequence(pair[0]['Sequence'])
-        seq2 = self.pad_sequence(pair[1]['Sequence'])
+        seq1 = one_hot_encode(pair[0]['Sequence'], self.max_seq_length)
+        seq2 = one_hot_encode(pair[1]['Sequence'], self.max_seq_length)
         feature1 = [v for k, v in pair[0].items() if k != 'Sequence']
         feature2 = [v for k, v in pair[1].items() if k != 'Sequence']
         if self.augment:
@@ -68,19 +82,12 @@ class RnaPairDataset(Dataset):
         else:
             # return (seq1, seq2, feature1, feature2)
             return (seq1, seq2)
-    
-    def pad_sequence(self, sequence):
-        # Pad sequence to max_seq_length with pad_token
-        if len(sequence) < self.max_seq_length:
-            sequence += self.pad_token * (self.max_seq_length - len(sequence))
-        else:
-            sequence = sequence[:self.max_seq_length]
-        return sequence
+
 
 def get_dataloaders(file_names=file_names, ADAR_types=ADAR_types, batch_size=32, train_ratio=0.8):
     dataset = RnaPairDataset(file_names, ADAR_types)
-    train_size = int(0.8 * len(dataset))
-    dev_size = int(0.1 * len(dataset))
+    train_size = int(train_ratio * len(dataset))
+    dev_size = int((1-train_ratio)/2 * len(dataset))
     test_size = len(dataset) - train_size - dev_size
     train_dataset, dev_dataset, test_dataset = random_split(dataset, [train_size, dev_size, test_size])
 
@@ -89,7 +96,7 @@ def get_dataloaders(file_names=file_names, ADAR_types=ADAR_types, batch_size=32,
     dev_dataset.dataset.augment = False   # Single direction for dev set
     test_dataset.dataset.augment = False  # Single direction for test set
 
-    train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
-    dev_loader = DataLoader(dev_dataset, batch_size=4, shuffle=False)
-    test_loader = DataLoader(test_dataset, batch_size=4, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    dev_loader = DataLoader(dev_dataset, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
     return train_loader, dev_loader, test_loader
